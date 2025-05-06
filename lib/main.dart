35dbe4a8344ef8e60c5d7fbcd033c942ca5302e9
guired13 <red2006135@gmail.com>
2025-05-06 15:55:17 +0800

    Implement theme toggle feature in MyApp and update HelloScreen to support dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,36 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  bool _isDarkTheme = false;
+
+  void _toggleTheme(bool newValue) {
+    setState(() {
+      _isDarkTheme = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hello Mobile!',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HelloScreen(),
+      theme: _isDarkTheme ? ThemeData.dark() : ThemeData.light(),
+      home: HelloScreen(onThemeChanged: _toggleTheme, isDarkTheme: _isDarkTheme),
     );
   }
 }
 
 class HelloScreen extends StatefulWidget {
+  final Function(bool) onThemeChanged;
+  final bool isDarkTheme;
+
+  HelloScreen({required this.onThemeChanged, required this.isDarkTheme});
+
   @override
   HelloScreenState createState() => HelloScreenState();
 }
@@ -40,17 +54,35 @@ class HelloScreenState extends State<HelloScreen> {
     });
   }
 
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Hello Mobile!'),
+        
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Dark Theme',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Switch(
+                  value: widget.isDarkTheme,
+                  onChanged: widget.onThemeChanged,
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
             Text('Enter your name:', style: TextStyle(fontSize: 16)),
             SizedBox(height: 8),
             TextField(
